8f69e764218bce1b1fbb7df39c5e9b25283dd47d
JiaLee0707 <s2018s37@e-mirim.hs.kr>
2020-06-10 14:00:47 +0900

    초기 커밋.

---
+++ InheritancePrac/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritancePrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Dog> Dogs = new List<Dog> { new Dog(), new Dog(), new Dog() };
+            List<Cat> Cats = new List<Cat> { new Cat(), new Cat(), new Cat() };
+
+            foreach( Dog dag in Dogs)
+            {
+                dag.Eat();
+                dag.Sleep();
+                dag.Bark();
+            }
+
+            foreach(Cat cat in Cats)
+            {
+                cat.Eat();
+                cat.Sleep();
+                cat.Meow();
+            }
+        }
+    }
+}
